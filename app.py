ad0c615611c2b763eca793de4f185d13d163bc0d
Jay Chung <jayychung9@gmail.com>
2020-03-29 15:28:12 -0700

    asdf

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, jsonify
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
-# import stockprediction
+import pandas as pd
 
 #Use flask_pymongo to set up mongo connection
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/stocks_app"
@@ -16,36 +14,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    stocks = mongo.db.stocks.insert_many()
-    return render_template("index.html"),  stocks=stocks)
+    #stocks = mongo.db.stocks.insert_many()
+    return render_template("index.html")
 
 @app.route('/predict')
-def data()
+def data():
     actual_df = pd.read_csv(f'actual_INX.csv')
     predicted_df = pd.read_csv(f'predicted_INX.csv')
-    future_df = pd.read_csv(f'future_INX.csv')
     start_date = '1/1/2018'
 
-    date_strings = (pd
-    .date_range(start=start_date, periods=len(predicted_df))
-    .strftime("%Y-%m-%d")
-    .values
-    .tolist())
+    # date_strings = (pd
+    #     .date_range(start=start_date, periods=len(predicted_df))
+    #     .strftime("%Y-%m-%d")
+    #     .values
+    #     .tolist())
 
-    actual_predicted_future = {
+    actual_and_predicted = {
 
         # x
-        'date': date_strings,
+        # 'date': date_strings,
         # y1
         'actual': actual_df['0'].tolist(),#to_json(orient='records'),
         # y2
         'predicted': predicted_df['0'].tolist()#to_json(orient='records')
-
-        'future': future_df['0'].tolist()#to_json(orient='records')
     } 
-    return jsonify(actual_predicted_future)
-
+    return jsonify(actual_and_predicted)
 
 
 if __name__ == '__main__':
+    app.debug = True 
     app.run()
